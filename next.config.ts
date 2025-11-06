5f24504af80daf584c195c0a69dd8d5fbb90ef13
marcosgperez <marcoss.g.perez@gmail.com>
2025-11-05 23:31:22 -0300

    Storybook deploys

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/storybook/:path*',
+        destination: 'https://tu-storybook-url.vercel.app/:path*',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
